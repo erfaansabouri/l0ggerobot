6ca63c599b9de4dd61b07fcfaaf6e8fac3131607
Erfan Sabouri <erfaansabouri@gmail.com>
2022-08-07 18:20:38 +0430

    Telegram Log

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Facade\FlareClient\Api;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class TelegramBotController extends Controller
 {
@@ -11,6 +12,8 @@ class TelegramBotController extends Controller
     {
         $telegram = new Api(env('TELEGRAM_BOT_TOKEN'));
         $update = $telegram->getUpdates();
+        Log::useDailyFiles(storage_path().'/logs/telegram.log');
+        Log::info($update);
         $chat_id = $update->getMessage()->getChat()->getId();
         $text = $update->getMessage()->getText();
         $reply = 'Hello, '.$text;

2e3d3e60f3605dcb4b462127f732487b61a5b988
Erfan Sabouri <erfaansabouri@gmail.com>
2022-08-07 17:59:59 +0430

    test

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Telegram\Bot\Traits\Telegram;
 
 
 Route::post('/endpoint/webhook', function () {
-    $update = Telegram::commandsHandler(true);
+    $update = \Telegram::commandsHandler(true);
 });
 
 Route::get('/setwebhook', function () {
-    $response = Telegram::setWebhook(['url' => 'https://l0ggerobot.viewshow.ir/endpoint/webhook']);
+    $response = \Telegram::setWebhook(['url' => 'https://l0ggerobot.viewshow.ir/endpoint/webhook']);
     dd($response);
 });

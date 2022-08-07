7444b49d01eb88ed067a72a675d2bc3962a95121
Erfan Sabouri <erfaansabouri@gmail.com>
2022-08-07 18:43:43 +0430

    Fix

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Crawlers\ShutterstockCrawler;
 use Illuminate\Support\Facades\Route;
+
+
 
 
 Route::post('/endpoint/webhook', [\App\Http\Controllers\TelegramBotController::class, 'handler']);
@@ -8,4 +11,14 @@ Route::post('/endpoint/webhook', [\App\Http\Controllers\TelegramBotController::c
 Route::get('/setwebhook', function () {
     $response = \Telegram::setWebhook(['url' => 'https://l0ggerobot.viewshow.ir/endpoint/webhook']);
     dd($response);
+});
+
+
+Route::get('/test', function () {
+    $crawler = new ShutterstockCrawler(env('SHUTTERSTOCK_API'));
+    $crawler->setQuery('boy');
+    $crawler->setPage(1);
+    $crawler->setPerPage(10);
+    $medias = $crawler->images();
+    dd($medias);
 });
